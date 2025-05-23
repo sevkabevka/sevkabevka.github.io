c8ca25ff5d3dc4139caeee37aa789cb50b4c8e65
sevkabevka <143031323+sevkabevka@users.noreply.github.com>
2025-05-23 23:09:28 +0300

    Delete tracker.js

--- tracker.js
+++
@@ -1,7 +0,0 @@
-fetch('https://discord.com/api/webhooks/1375557655036301593/fAVFnmHle5XCW4nxYD3GKHetyJRbGyQa6IXia2LbrEmk4Sn6Gaym_YduQpvpgkD2h0oA', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ content: "Webhook test from browser console" }),
-})
-.then(() => console.log("Webhook test sent"))
-.catch(e => console.error("Webhook test failed:", e));
